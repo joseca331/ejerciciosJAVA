b85394e790683f1dd78b4298b186cf5d294db52e
Emilio <carbo.albelda.emilio@iescamas.es>
2025-10-03 17:05:25 +0200

    ejercicio12

--- EjerciciosJAVA/ActividadesU2/src/actividades/Actividad12.java
+++ EjerciciosJAVA/ActividadesU2/src/actividades/Actividad12.java
@@ -8,7 +8,7 @@ public class Actividad12 {
 
 		 int dia,mes,año;
 		 
-		 
+		 //Solicita al usuario que introduzca el día, mes y año
 	        System.out.print("Introduce el día: ");
 	         dia = sc.nextInt();
 
@@ -18,19 +18,28 @@ public class Actividad12 {
 	        System.out.print("Introduce el año: ");
 	         año = sc.nextInt();
 
+	         // Variable para indicar si la fecha es válida
 	        boolean fechaCorrecta = false;
 
 	        if (año >= 0) {
 	            switch (mes) {
+	            // Meses con 31 días
+	           
 	                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
 	                    if (dia >= 1 && dia <= 31) fechaCorrecta = true;
 	                    break;
+	                    
+	                 // Meses con 30 días
 	                case 4: case 6: case 9: case 11:
 	                    if (dia >= 1 && dia <= 30) fechaCorrecta = true;
 	                    break;
+	                    
+	                    // Febrero 
 	                case 2:
 	                    if (dia >= 1 && dia <= 28) fechaCorrecta = true;
 	                    break;
+	                    
+	                    // Mes inválido
 	                default:
 	                    fechaCorrecta = false;
 	            }
